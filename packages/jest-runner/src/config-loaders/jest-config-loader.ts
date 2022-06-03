99f153f56332e4c144aadd65ec8480465775b843
KasNotten <kasnotten1@hotmail.com>
2022-06-03 21:02:19 +0200

    improve documenting comments

--- packages/jest-runner/src/config-loaders/jest-config-loader.ts
+++ packages/jest-runner/src/config-loaders/jest-config-loader.ts
@@ -1,9 +1,9 @@
 /**
- * The ConfigLoader interface is used to load different kinds of configurations for Jest.
- * Custom ConfigLoaders should implement this interface, the OptionsEditor will then be able to use it to load a Jest configuration.
+ * The JestConfigLoader interface is used to load different kinds of configurations for Jest.
+ * Custom JestConfigLoaders should implement this interface, the OptionsEditor will then be able to use it to load a Jest configuration.
  *
- * ConfigLoaders are typically used for projects that do not provide their configuration via the package.json file (e.g. React).
- * The loaderConfig method will return a usable config for Jest to use.
+ * JestConfigLoaders are typically used for projects that do not provide their configuration via the package.json file (e.g. React).
+ * The loadConfig method will return a usable config for Jest to use.
  */
 
 import { Config } from '@jest/types';
